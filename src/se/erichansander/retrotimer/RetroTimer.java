c3cfacff32d5a4eaa46f555f31370509bf6d7a58
Eric Hansander <ehdr@erichansander.se>
2010-09-16 21:25:58 +0200

    Initial commit.

---
+++ src/se/erichansander/retrotimer/RetroTimer.java
@@ -0,0 +1,13 @@
+package se.erichansander.retrotimer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RetroTimer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
