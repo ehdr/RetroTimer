af47b29eac55f188a50a1589ba09f0135d46cddc
Eric Hansander <ehdr@erichansander.se>
2010-12-12 19:40:31 +0100

    Change app name to "Retro Timer"

--- src/se/erichansander/retrotimer/RetroTimer.java
+++ src/se/erichansander/retrotimer/RetroTimer.java
@@ -1,20 +1,20 @@
 /* 
  * Copyright (C) 2010  Eric Hansander
  *
- *  This file is part of RetroTimer.
+ *  This file is part of Retro Timer.
  *
- *  RetroTimer is free software: you can redistribute it and/or modify
+ *  Retro Timer is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  RetroTimer is distributed in the hope that it will be useful,
+ *  Retro Timer is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with RetroTimer.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Retro Timer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package se.erichansander.retrotimer;
@@ -148,7 +148,7 @@ public class RetroTimer extends Application {
      */
     public static void setAlarmAt(Context context, long alarmTime) {
     	if (RetroTimer.DEBUG) {
-    		Elog.d(DEBUG_TAG, "RetroTimer alarm set for " +
+    		Elog.d(DEBUG_TAG, "Alarm set to trigger at " +
     				DateFormat.format("hh:mm:ss", alarmTime) +
     				" (now is " +
     				DateFormat.format("hh:mm:ss", System.currentTimeMillis()) +
--- src/se/erichansander/retrotimer/TimerView.java
+++ src/se/erichansander/retrotimer/TimerView.java
@@ -1,20 +1,20 @@
 /* 
  * Copyright (C) 2010  Eric Hansander
  *
- *  This file is part of RetroTimer.
+ *  This file is part of Retro Timer.
  *
- *  RetroTimer is free software: you can redistribute it and/or modify
+ *  Retro Timer is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  RetroTimer is distributed in the hope that it will be useful,
+ *  Retro Timer is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with RetroTimer.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Retro Timer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package se.erichansander.retrotimer;
